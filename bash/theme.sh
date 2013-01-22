c01eea0c0c68d7935b8536414cf7788e9616d34c
Mark Godwin <godwin.mark@gmail.com>
2013-01-22 11:15:48 -0700

    Prompt is now SSH aware

--- bash/theme.sh
+++ bash/theme.sh
@@ -4,6 +4,14 @@
 source "$DOTFILES/bash/bash-it.color.theme"
 source "$DOTFILES/bash/bash-it.base.theme"
 
+function ssh_info {
+  if [ ! -z "$SSH_CONNECTION" ]; then
+    echo -e "\h:"
+  fi
+}
+
+ssh_connection_info="$(ssh_info)"
+
 #added TITLEBAR for updating the tab and window titles with the pwd
 case $TERM in
   xterm*)
@@ -13,7 +21,7 @@ case $TERM in
   TITLEBAR=""
   ;;
 esac
-PROMPT="${TITLEBAR}${orange}${reset_color}${green}\w${bold_blue}\$(scm_prompt_info)${reset_color}${bold_red}\$(ruby_version_prompt)${reset_color}${normal} "
+PROMPT="${TITLEBAR}${white}${ssh_connection_info}${reset_color}${green}\w${bold_blue}\$(scm_prompt_info)${reset_color}${bold_red}\$(ruby_version_prompt)${reset_color}${normal} "
 
 
 # scm themeing
