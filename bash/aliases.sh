2c79618370bdbdeb3496ee5ec7d5159d67ad599f
Mark Godwin <godwin.mark@gmail.com>
2013-01-25 11:24:47 -0700

    Grep fixes

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,1 +1,8 @@
-alias reload='source ~/.bashrc'
+alias reload='unalias -a && source ~/.bashrc'
+function g {
+  if [ -z "$1" ];then
+    echo "Must specify something to search for"
+    return 1;
+  fi
+  grep -rin --color "$@" .
+}
