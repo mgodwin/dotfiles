bb6cf65f65760663429933768625bd539ec1e90f
Mark Godwin <mark.godwin@logikcull.com>
2018-01-09 17:53:32 +0000

    Small updates for linux more vim

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
+if [ "$(uname)" == "Darwin" ]; then
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-which -s brew
-if [[ $? != 0 ]] ; then
-  # Install Homebrew
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  which -s ansible-playbook
+  if [[ $? != 0 ]] ; then
+    # Install ansible
+    brew install ansible
+  fi
+
+  ansible-playbook -i local setup.yml
+else
+  echo "Unsupported OS, please run from a Mac - you can use SSH to setup linux from mac"
 fi
-
-which -s ansible-playbook
-if [[ $? != 0 ]] ; then
-  # Install ansible
-  brew install ansible
-fi
-
-ansible-playbook -i local setup.yml
